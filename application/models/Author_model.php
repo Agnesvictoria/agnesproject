e5cda97c5bff283d0223ee1fffb0d9cdf43c2dc0
Agnesvictoria <agnesvictoria17@gmail.com>
2025-10-03 21:40:01 +0700

    Create Author_model.php

---
+++ application/models/Author_model.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Author_model extends CI_Model {
+    private $table = "authors";
+
+    public function get_all(){
+        return $this->db->get($this->table)->result();
+    }
+
+    public function get_by_id($id){
+        return $this->db->get_where($this->table, ['id'=>$id])->row();
+    }
+
+    public function insert($data){
+        return $this->db->insert($this->table, $data);
+    }
+
+    public function update($id, $data){
+        return $this->db->where('id',$id)->update($this->table,$data);
+    }
+
+    public function delete($id){
+        return $this->db->delete($this->table, ['id'=>$id]);
+    }
+}
