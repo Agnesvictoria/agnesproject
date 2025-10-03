2db6b6b4d6113c04d31a92c7c1dd13b92cbec695
Agnesvictoria <agnesvictoria17@gmail.com>
2025-10-03 21:42:31 +0700

    Create sidebar.php

---
+++ application/views/admin/templates/sidebar.php
@@ -0,0 +1,11 @@
+<div class="sidebar">
+    <h3>Admin</h3>
+    <a href="<?= site_url('admin/dashboard'); ?>">DASHBOARD</a>
+    <a href="<?= site_url('web'); ?>">HALAMAN WEB</a>
+    <a href="<?= site_url('admin/artikel') ?>">ARTIKEL</a>
+    <a href="<?= site_url('admin/categories') ?>">Data Kategori</a>
+    <a href="<?= site_url('admin/tags') ?>">Data Tag</a>
+    <a href="<?= site_url('admin/authors') ?>">Data Author</a>
+    <a href="<?= site_url('auth/logout') ?>">Logout</a>
+</div>
+<div class="content">
